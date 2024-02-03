3acf3514340cda942b468458f6c67c20e0903cf3
victor nta <victornta32@gmail.com>
2024-02-03 14:55:04 +0100

    fixed pylint bug

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,4 +1,4 @@
-from nlplogic.corenlp import search_wikipedia,summarize_wikipedia,get_text_blob,get_phrase_blob
+from nlplogic.corenlp import get_phrase_blob
 
 def test_get_phrase():
     assert 'golden state' in get_phrase_blob('Golden State Warriors')
